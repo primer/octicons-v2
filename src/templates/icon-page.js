287593b8a172ad56dc0c12be35ebc0db1f27f882
Cole Bemis <colebemis@github.com>
2019-10-16 12:27:56 -0700

    Implement "Download PDF"

--- src/templates/icon-page.js
+++ src/templates/icon-page.js
@@ -1,7 +1,10 @@
 /** @jsx jsx */
+import blobStream from "blob-stream"
 import copy from "copy-to-clipboard"
 import download from "downloadjs"
+import PDFDocument from "pdfkit/js/pdfkit.standalone"
 import React from "react"
+import svgToPdf from "svg-to-pdfkit"
 import { jsx } from "theme-ui"
 import Button from "../components/button"
 import Head from "../components/head"
@@ -13,7 +16,12 @@ import Specimens24 from "../components/specimens-24"
 
 export default function IconPage({ pageContext }) {
   const { name, width, height, viewBox, contents } = pageContext
-  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="${viewBox}" width="${width}" height="${height}">${contents}</svg>`
+  const svg = getSvg({ viewBox, width, height, contents })
+  const [pdf, setPdf] = React.useState(null)
+
+  React.useEffect(() => {
+    getPdf({ svg, width, height }).then(blob => setPdf(blob))
+  }, [svg, width, height])
 
   const [copied, setCopied] = React.useState(false)
 
@@ -59,7 +67,12 @@ export default function IconPage({ pageContext }) {
         <Button onClick={() => download(svg, `${name}.svg`, "image/svg+xml")}>
           Download SVG
         </Button>
-        <Button disabled>Download PDF</Button>
+        <Button
+          disabled={!pdf}
+          onClick={() => download(pdf, `${name}.pdf`, "application/pdf")}
+        >
+          Download PDF
+        </Button>
       </div>
       <h2 sx={{ mt: 4, mb: 3, fontWeight: "bold", fontSize: 4 }}>Specimens</h2>
       <Specimens
@@ -87,4 +100,20 @@ function Specimens({ size, icon }) {
     default:
       return <p sx={{ mt: 0, mb: 3 }}>No specimens available</p>
   }
+}
+
+function getSvg({ viewBox, width, height, contents }) {
+  return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="${viewBox}" width="${width}" height="${height}">${contents}</svg>`
+}
+
+function getPdf({ svg, width, height }) {
+  return new Promise(resolve => {
+    const doc = new PDFDocument({ size: [width, height] })
+    const stream = doc.pipe(blobStream())
+    svgToPdf(doc, svg, 0, 0, { assumePt: true })
+    doc.end()
+    stream.on("finish", function() {
+      resolve(stream.toBlob("application/pdf"))
+    })
+  })
 }
