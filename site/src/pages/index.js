63a4ec224028aeb123fcf2bcf51ccc404c9775b0
Cole Bemis <colebemis@github.com>
2019-12-12 13:50:51 -0800

    Implement search (#134)
    * Implement search
    
    * Use Input component from theme-ui

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,11 +1,14 @@
 /** @jsx jsx */
+import { Input } from "@theme-ui/components"
+import Tooltip from "@tippy.js/react"
 import { graphql, Link, useStaticQuery } from "gatsby"
 import groupBy from "lodash.groupby"
+import React from "react"
 import { jsx } from "theme-ui"
-import Layout from "../components/layout"
 import Head from "../components/head"
-import Tooltip from "@tippy.js/react"
 import Icon from "../components/icon"
+import Layout from "../components/layout"
+import useSearch from "../use-search"
 
 export default function App() {
   const data = useStaticQuery(graphql`
@@ -21,8 +24,10 @@ export default function App() {
       }
     }
   `)
+  const [query, setQuery] = React.useState("")
+  const results = useSearch(data.allIcon.nodes, query, { keys: ["name"] })
 
-  const iconsBySize = groupBy(data.allIcon.nodes, "size")
+  const iconsBySize = groupBy(results, "size")
 
   return (
     <Layout>
@@ -33,41 +38,52 @@ export default function App() {
           gridGap: 5,
         }}
       >
-        {Object.entries(iconsBySize).map(([size, icons]) => (
-          <div key={size}>
-            <h2 sx={{ mt: 0, mb: 4, fontSize: 2, fontWeight: "bold" }}>
-              {size}px
-            </h2>
-            <div
-              sx={{
-                display: "grid",
-                gridGap: 4,
-                gridTemplateColumns: `repeat(auto-fill, ${size}px)`,
-              }}
-            >
-              {icons.map(icon => (
-                <Tooltip
-                  key={icon.slug}
-                  content={icon.name}
-                  arrow={true}
-                  arrowType="round"
-                  delay={300}
-                >
-                  <Link
-                    to={`/${icon.slug}`}
-                    sx={{ display: "flex", color: "inherit" }}
+        <Input
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+          type="search"
+          placeholder="Search icons..."
+          sx={{ p: 3, bg: "muted", borderColor: "border" }}
+        />
+        {Object.entries(iconsBySize).length > 0 ? (
+          Object.entries(iconsBySize).map(([size, icons]) => (
+            <div key={size}>
+              <h2 sx={{ mt: 0, mb: 4, fontSize: 2, fontWeight: "bold" }}>
+                {size}px
+              </h2>
+              <div
+                sx={{
+                  display: "grid",
+                  gridGap: 4,
+                  gridTemplateColumns: `repeat(auto-fill, ${size}px)`,
+                }}
+              >
+                {icons.map(icon => (
+                  <Tooltip
+                    key={icon.slug}
+                    content={icon.name}
+                    arrow={true}
+                    arrowType="round"
+                    delay={300}
                   >
-                    <Icon
-                      size={icon.size}
-                      viewBox={icon.viewBox}
-                      contents={icon.contents}
-                    />
-                  </Link>
-                </Tooltip>
-              ))}
+                    <Link
+                      to={`/${icon.slug}`}
+                      sx={{ display: "flex", color: "inherit" }}
+                    >
+                      <Icon
+                        size={icon.size}
+                        viewBox={icon.viewBox}
+                        contents={icon.contents}
+                      />
+                    </Link>
+                  </Tooltip>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p sx={{ textAlign: "center" }}>No results found</p>
+        )}
       </div>
     </Layout>
   )
