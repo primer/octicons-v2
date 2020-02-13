decdcf692a435487a20022a8a1c5e3dcd6422e52
Cole Bemis <colebemis@github.com>
2020-02-13 15:22:20 -0800

    Add /octicons-v2 path prefix (#225)

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     imageUrl:
       "https://user-images.githubusercontent.com/10384315/53922681-2f6d3100-402a-11e9-9719-5d1811c8110a.png",
   },
+  pathPrefix: "/octicons-v2",
   plugins: [
     "gatsby-plugin-theme-ui",
     "gatsby-plugin-react-helmet",
